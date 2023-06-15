6bc0e470f57b671a2a93c74918809917a5ff63eb
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-15 15:17:59 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this function for printing that text
+ * @void: this is the barometer for the function
+ * puts(): this code line for print that text
+ * return: it's for taking back result 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
