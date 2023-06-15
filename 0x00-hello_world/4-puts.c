130595c6f1388a50c9be94f0aaedac1686fe113b
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-15 16:48:26 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  * return: 0 (Success)
  */
-int main(void)
+int main(void) /*a function to output the current text*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
