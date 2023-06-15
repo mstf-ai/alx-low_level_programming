d2ffa3a40f401c418dae26bc007c61fad2484d2a
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-15 16:57:45 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void) /*main - a C program that prints the size of various types on the computer it is compiled and run on*/
+/**
+ * main - a C program that prints the size of various types
+ *on the computer it is compiled and run on
+ * return: This return 0 (Success)
+ */
+int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
