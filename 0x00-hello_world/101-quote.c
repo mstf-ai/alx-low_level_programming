dd65efae677a980f3df66108932f8d5d75a5069c
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-15 17:06:23 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/*
+ * main - this is a function to print text in new line
+ *
+ * retorn: this to retorn 1 always
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
