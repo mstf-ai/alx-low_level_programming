0abf208ab4fafcf545a69cfb64247c76e5a4c011
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-16 12:43:30 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - A program that a line with puts function
- *
- * return: 0 (Success)
- */
-int main(void) /*a function to output the current text*/
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
