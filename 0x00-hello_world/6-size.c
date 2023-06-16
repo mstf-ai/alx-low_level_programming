1726474af8c2433b71f0cb96f56f6ce45e7353df
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-16 13:46:41 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-
 /**
- * main - a C program that prints the size of various types
- * on the computer it is compiled and run on
+ * main - a function named main
  *
  * return: This return 0 (Success)
  */
