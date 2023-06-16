4ed608c4283a2d5b7e905a80ef254caecd768f6d
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-15 17:11:39 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/*
+ * main -  a C program that prints this text line followed by a new line.
+ * return: Always returns 0 (Success)
+ */
 int main(void)
 {
 printf ("with proper grammar, but the outcome is a piece of art,\n");
