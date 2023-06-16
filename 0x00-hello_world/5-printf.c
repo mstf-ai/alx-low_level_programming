65c3c92c4e87d785cddc9079dc9eff94c433c582
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-06-16 13:20:43 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /*
  * main -  a C program that prints this text line followed by a new line.
- * return: Always returns 0 (Success)
+ * Return: Always returns 0 (Success)
  */
 int main(void)
 {
