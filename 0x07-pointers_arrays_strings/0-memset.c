0cd60be89474918b633e6fd8073f598fcc42f4a1
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-07-03 07:57:14 -0700

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
-
-	for (; n > 0; i++)
-	{
-		s[i] = b;
-		n--;
-	}
-	return (s);
+    unsigned int i;
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
+    return s;
 }
+
