f6aadde2bbc6100a812d6ac0dd06cd3734ca48f4
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-07-20 04:55:49 -0700

    a

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings with separator
+ * @separator: hte string separator
+ * @n: the number of arguments
+ * @...: the strings to print 
+ *
+ * Return: void
+ */
+
