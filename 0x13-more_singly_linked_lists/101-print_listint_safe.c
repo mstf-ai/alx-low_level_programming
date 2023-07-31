af04b390fbf49793c39cb8edc68cabaf4e605791
MOSTAFA-Web Developer <albrnsmstfy252@gmail.com>
2023-07-31 12:37:39 -0700

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,33 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+const listint_t **_r(const listint_t **list, size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0, i < size - 1, i++)
+		newlist[i] - list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
 
 /**
  * reverse_listint - prints a listint_t linked list.
@@ -7,17 +36,25 @@
  **/
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t pichu = 0;
-	const listint_t *aux_node = head;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
-	if (!head)
-		exit(98);
-
-	while (aux_node)
+	while (head != NULL)
 	{
-		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
-		aux_node = aux_node->next;
-		pichu++;
+		for(i = 0, i < num, i++)
+		{
+			if (head == list[i])
+			{
+				printif("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _r(list, num, head);
+		printif("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-	return (pichu);
+	free(list);
+	return (num);
 }
